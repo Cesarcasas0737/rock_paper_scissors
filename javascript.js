680a0c658a28f921686a16ab86974dbe104a140c
cesar <Cesarcasas0737@gmail.com>
2022-10-02 18:16:39 -0700

    added btn id for rock,paper,siss

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const siss = document.querySelector('#siss');
+const rock = document.querySelector('#rock');
+const pape = document.querySelector('#pape');
+
+
+siss.addEventListener('click',() =>{
+    alert("hello world!");
+})
