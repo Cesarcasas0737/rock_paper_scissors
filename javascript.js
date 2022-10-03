c1793fb85ee4c9c586f0b689d178a8e2344ffe94
cesar <Cesarcasas0737@gmail.com>
2022-10-02 18:29:22 -0700

    changed id's for a universal user choice

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
-const siss = document.querySelector('#siss');
-const rock = document.querySelector('#rock');
-const pape = document.querySelector('#pape');
+const choice = document.querySelector('#picked');
+
 
 
 siss.addEventListener('click',() =>{
