0797cf75f673ad8741f47f7a71f85b5f987ecc0e
cesar <Cesarcasas0737@gmail.com>
2022-10-12 10:26:35 -0700

    Redoing javascript logic and html ID

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
-const choice = document.querySelector('#picked');
+/*need to make rounds
+how computer chooses
+player chooses 
 
+array that holds [rock,paper, sciccors]
+*/
+var rounds = 5;
+var userChoice ="NO VALUE";
+var computerChoice ="NO VALUE";
 
+const chooseRPS = ["rock","paper","Scissors"];
 
-siss.addEventListener('click',() =>{
-    alert("hello world!");
-})
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function getComChoice(){
+    computerChoice = chooseRPS[getRandomInt(3)];
+    console.log("Local choice from function" + computerChoice);
+}
+getComChoice();
